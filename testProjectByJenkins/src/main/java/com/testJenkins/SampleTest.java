4e08e810e3a93e0f4a224313b01fe76bd7fd27d2
Suhas <suhas2229@gmail.com>
2021-03-15 12:49:46 +0530

    first commit

---
+++ testProjectByJenkins/src/main/java/com/testJenkins/SampleTest.java
@@ -0,0 +1,20 @@
+package com.testJenkins;
+
+public class SampleTest {
+	
+	public void createContact()
+	{
+		System.out.println("Create contact");
+	}
+	
+	public void modifyContact()
+	{
+		System.out.println("Modify contact");
+	}
+
+	public void deleteContact()
+	{
+		System.out.println("Delete contact");
+	}
+
+}
